8ad1149589a280e30b5dd43a922ebfbee943ea85
Lucas Duarte <lucasduarte599@gmail.com>
2017-01-28 10:03:58 -0200

    Configs models

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,16 @@
 class Search < ApplicationRecord
+  has_many :user_searches
+  validates_uniqueness_of :search
+
+  def self.related_searches(search)
+    where('search LIKE ?', "#{search}%")
+      .order('user_searches_count DESC')
+      .pluck(:search)
+  end
+
+  def self.get_by_prhase(search_phrase)
+    select(:id)
+      .find_by(search: search_phrase)
+      .id
+  end
 end
--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -1,2 +1,15 @@
 class UserSearch < ApplicationRecord
+  belongs_to :search, counter_cache: true
+
+  def self.search(search_phrase)
+
+    search = Search.new(search: search_phrase)
+
+    unless search.save
+      search_id = Search.get_by_prhase(search_phrase)
+      create(search_id: search_id)
+    else
+      create(search_id: search.id)
+    end
+  end
 end
